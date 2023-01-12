610119ec7d49cf6e4bc4de30be9d149b6fbe1a8a
GIT-TPY <tcdbz123@gmail.com>
2023-01-12 14:14:10 +0800

    v1.0
    修复目录问题

--- img/README.md
+++
@@ -1,3 +0,0 @@
-# 小驿站
-
-源站点 https://www.yuque.com/u32097440/xyz
--- img/js/sidebar.js
+++
@@ -1,8 +0,0 @@
-function openSideBar(){
-    document.getElementById("sideBar").style.width = "15%";
-    document.getElementById("sideBar").style.boxShadow = "0px 1px 10px 5px #c4c9ce"; //boxShadow用于添加边框阴影
-}
-function closeSideBar(){
-    document.getElementById("sideBar").style.width = "0%";
-    document.getElementById("sideBar").style.boxShadow = "0px 0px 0px 0px #c4c9ce";
-}
